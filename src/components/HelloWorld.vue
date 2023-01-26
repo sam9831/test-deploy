9df452d3857dfb8b37d824867b3492be4265b3c9
sam <247765564@qq.com>
2023-01-27 01:22:23 +0800

    feat: update jenkins

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,7 +5,8 @@
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.<br>
-      hello imooc
+      hello imooc<br>
+      hello jenkins
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
