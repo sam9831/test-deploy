9442a3147cc7c67fc8258433385df5fba6a645c2
sam <247765564@qq.com>
2023-01-27 09:44:22 +0800

    feat: add docker

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,8 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.<br>
       hello imooc<br>
-      hello jenkins
+      hello jenkins<br>
+      hello docker
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
